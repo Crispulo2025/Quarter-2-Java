98652a66d2b309f48815bae65be6edf2ede01654
Crispulo2025 <marpalace1@gmail.com>
2025-12-09 16:09:23 +0800

    Add files via upload

---
+++ RemainderBySeven.java
@@ -0,0 +1,12 @@
+public class RemainderBySeven {
+    public static void main(String[] args) {
+        // Declare an integer
+        int number = 50;
+
+        // Calculate the remainder when divided by 7
+        int remainder = number % 7;
+
+        // Display the result
+        System.out.println("The remainder when " + number + " is divided by 7 is " + remainder);
+    }
+}
