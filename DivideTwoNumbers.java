f46fdca92a3940323cb3107c92ec09320a8415ff
Crispulo2025 <marpalace1@gmail.com>
2025-12-09 15:27:08 +0800

    Add files via upload

---
+++ DivideTwoNumbers.java
@@ -0,0 +1,17 @@
+public class DivideTwoNumbers {
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int dividend = 25;
+        int divisor = 4;
+
+        // Calculate quotient and remainder
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        // Display the results
+        System.out.println("Dividend: " + dividend);
+        System.out.println("Divisor: " + divisor);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
