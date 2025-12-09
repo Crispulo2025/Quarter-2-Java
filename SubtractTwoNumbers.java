a9aaca6d5e09a74d97f5506ff19fe572a1205f05
Crispulo2025 <marpalace1@gmail.com>
2025-12-09 14:53:11 +0800

    Add files via upload

---
+++ SubtractTwoNumbers.java
@@ -0,0 +1,13 @@
+public class SubtractTwoNumbers {
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int num1 = 25;
+        int num2 = 10;
+
+        // Subtract num2 from num1
+        int difference = num1 - num2;
+
+        // Display the result
+        System.out.println("The difference between " + num1 + " and " + num2 + " is " + difference);
+    }
+}
