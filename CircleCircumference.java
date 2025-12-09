f849f47b07441f67048c49fc02194fd22bf91847
Crispulo2025 <marpalace1@gmail.com>
2025-12-09 15:59:19 +0800

    Add files via upload

---
+++ CircleCircumference.java
@@ -0,0 +1,12 @@
+public class CircleCircumference {
+    public static void main(String[] args) {
+        // Declare the radius
+        double radius = 7.0;
+
+        // Use Math.PI for a precise value of pi
+        double circumference = 2 * Math.PI * radius;
+
+        // Display the result
+        System.out.println("The circumference of a circle with radius " + radius + " is " + circumference);
+    }
+}
