af412784d6f0d3169434a8e084d924da05dac4dc
Crispulo2025 <marpalace1@gmail.com>
2025-12-09 15:20:41 +0800

    Add files via upload

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,13 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int num1 = 7;
+        int num2 = 5;
+
+        // Multiply the two numbers
+        int product = num1 * num2;
+
+        // Display the result
+        System.out.println("The product of " + num1 + " and " + num2 + " is " + product);
+    }
+}
