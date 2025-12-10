93558dc8323bda0a910d6ca48437eafb72fc7755
Crispulo2025 <marpalace1@gmail.com>
2025-12-10 12:09:35 +0800

    Add files via upload

---
+++ LessOrEqualCheck.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LessOrEqualCheck {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input the first number
+        System.out.print("Enter the first number: ");
+        int first = input.nextInt();
+
+        // Input the second number
+        System.out.print("Enter the second number: ");
+        int second = input.nextInt();
+
+        // Check if first is less than or equal to second
+        if (first <= second) {
+            System.out.println("The first number is less than or equal to the second.");
+        } else {
+            System.out.println("The first number is greater than the second.");
+        }
+
+        input.close();
+    }
+}
