48f1782a875c59ce338523bd434877e2f246cc06
Crispulo2025 <marpalace1@gmail.com>
2026-01-29 10:39:57 +0800

    Add files via upload

---
+++ MergeHelper.java
@@ -0,0 +1,43 @@
+// Method to merge two sorted halves of an array
+public static void merge(int[] arr, int left, int mid, int right) {
+    int n1 = mid - left + 1; // Size of left half
+    int n2 = right - mid;    // Size of right half
+
+    // Temporary arrays
+    int[] L = new int[n1];
+    int[] R = new int[n2];
+
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    // Merge temp arrays back into original array
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
