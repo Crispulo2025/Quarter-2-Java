de3e3c2432bd4e28ce9d95dfa6c5e7cf321bf9d2
Crispulo2025 <marpalace1@gmail.com>
2026-02-04 07:54:52 +0800

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user for a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        scanner.close();
+    }
+}
